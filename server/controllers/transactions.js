36ba2e69d72671d0837822dcc92627fdb0b45094
SharjunA <sharjun.sh786@gmail.com>
2024-02-04 02:02:36 +0530

    ON BOARD PICS

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -26,31 +26,11 @@ export const createTransaction = async (req, res) => {
       order_id,
     });
 
-    const savedUser = await newUser.save();
-    res.status(201).json(savedUser);
+    const savedTransaction = await newTransaction.save();
+    res.status(201).json(savedTransaction);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-/* LOGGING IN */
-export const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    console.log(email,password);
-    const user = await User.findOne({ email: email });
-    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    delete user.password;
-
-    console.log(user.firstName + " " + user.lastName, " logined Successfully");
-    console.log(token,user);
-    res.status(200).json({ token, user });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
