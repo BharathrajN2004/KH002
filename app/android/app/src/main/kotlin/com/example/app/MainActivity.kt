03095102e6b0ac32c76ed61ffcd6fb15382f6590
SharjunA <sharjun.sh786@gmail.com>
2024-02-04 08:54:51 +0530

    biometric completed

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.app
+package com.example.flutter_tricks
 
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity: FlutterFragmentActivity() {
 }
