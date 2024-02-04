6644bd247ea14b61a90a079d36b349e880cbcab2
SharjunA <sharjun.sh786@gmail.com>
2024-02-04 09:28:20 +0530

    Biometric updation

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++ app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
@@ -1,7 +1,6 @@
-package com.example.flutter_tricks
+package com.example.app
 
+import io.flutter.embedding.android.FlutterActivity
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
 }
